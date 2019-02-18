41cddcf4608f68e448bc69e3964baf132d7711aa
scary <imbamrgrey@gmail.com>
2019-02-19 00:31:33 +0100

    init

---
+++ Microsoft.Architecture.Catalog.Api/Database/CatalogContex.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Architecture.Catalog.Api.Database
+{
+    public class CatalogContext : DbContext
+    {
+
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+        }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+
+        
+    }
+}
---
+++ Microsoft.Architecture.Catalog.Api/Database/CatalogType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Architecture.Catalog.Api.Database
+{
+    public class CatalogType
+    {
+    }
+}
